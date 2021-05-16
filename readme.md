603ee287db373e74f55ba0834229a0b16e553c22
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-05-16 11:41:06 +0900

    readme.md: replaced backslashes to the forward slashes as path separator

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 Articles
 =========
 
-* ["Windows と Makefile とボク"](articles\9f2a5d1030943d.md)
-* ["「コマンドラインシェル？？？　誰でも作れますよ」"](articles\d7b76ff6535d7d.md)
+* ["Windows と Makefile とボク"](articles/9f2a5d1030943d.md)
+* ["「コマンドラインシェル？？？　誰でも作れますよ」"](articles/d7b76ff6535d7d.md)
 
 Books
 ======
 
-* ["scoop / nyagos で始めるコマンドライン生活"](books\5ac80a9ddb35fef9a146/readme.md)
-* ["Windows と Unicode とボク"](books\b820d588f4856bcf836c/readme.md)
-* ["/bin/shに慣れた人に贈るバッチファイルの書き方"](books\c84cbe23093eee1b5830/readme.md)
+* ["scoop / nyagos で始めるコマンドライン生活"](books/5ac80a9ddb35fef9a146/readme.md)
+* ["Windows と Unicode とボク"](books/b820d588f4856bcf836c/readme.md)
+* ["/bin/shに慣れた人に贈るバッチファイルの書き方"](books/c84cbe23093eee1b5830/readme.md)
--- update.cmd
+++ update.cmd
@@ -3,11 +3,11 @@
   echo =========
   echo.) > readme.md
 
-for %%I in (articles\*.md) do goawk "match($0,/title: /){ printf '* [%%s](%%s)\n',substr($0,RSTART+RLENGTH),FILENAME }" %%I >> readme.md
+for %%I in (articles\*.md) do goawk -v "fname=%%I" "BEGIN{ gsub(/\\/,'\/',fname) } match($0,/title: /){ printf '* [%%s](%%s)\n',substr($0,RSTART+RLENGTH),fname}" %%I >> readme.md
 
 ( echo.
   echo Books
   echo ======
   echo.) >> readme.md
 
-for /D %%I in (books\*) do goawk -v "dir=%%I" "match($0,/title: /){ printf '* [%%s](%%s/readme.md)\n',substr($0,RSTART+RLENGTH),dir }" %%I\config.yaml >> readme.md
+for /D %%I in (books\*) do goawk -v "dir=%%I" "BEGIN{ gsub(/\\/,'\/',dir)} match($0,/title: /){ printf '* [%%s](%%s/readme.md)\n',substr($0,RSTART+RLENGTH),dir}" %%I\config.yaml >> readme.md
